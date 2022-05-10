148da4df50a52c3ae31c5fa412dd7010e825b1ae
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-05-10 13:02:41 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Problem 6: Sum Square Difference.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 def solution
-  a = 1
-  b = 2
-  c = []
-
-  while a < 4_000_000
-    c.push(a)
-    a, b = b, a + b
-  end
-
-  c.select(&:even?).sum
+  sum_of_squares = (1..100).sum { |x| x**2 }
+  square_of_sum = (1..100).sum**2
+  (sum_of_squares - square_of_sum).abs
 end
+
+# Alternate solution using variables
+
+# def solution
+#   sum_of_squares = 0
+
+#   (1..100).each { |x| sum_of_squares += x**2 }
+
+#   square_of_sum = 0
+
+#   (1..100).each { |x| square_of_sum += x }
+
+#   square_of_sum **= 2
+
+#   (sum_of_squares - square_of_sum).abs
+# end
