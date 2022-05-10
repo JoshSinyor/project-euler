1667b33c597920491964efb7a7e105f46da58231
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-05-10 12:25:30 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Problem 1: Multiples of 3 or 5.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 def solution
+  (0...1000).select { |x| (x % 3).zero? || (x % 5).zero? }.sum
 end
+
+# Alternative solution using variable
+
+# def solution
+#   sum_of_multiples = 0
+
+#   (0...1000).each { |x| sum_of_multiples += x if (x % 3).zero? || (x % 5).zero? }
+
+#   sum_of_multiples
+# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -2,5 +2,12 @@
 
 require 'solution'
 
+# Problem 1: Multiples of 3 or 5
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 describe 'solution' do
+  it 'returns the sum of all multiples of 3 or 5 below 1000' do
+    expect(solution).to eq 233_168
+  end
 end
