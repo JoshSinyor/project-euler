3f468f7e269ab2104f4322e021cb594c4e3b2f47
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-05-10 12:40:53 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Problem 2: Even Fibonacci Numbers.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 def solution
-  (0...1000).select { |x| (x % 3).zero? || (x % 5).zero? }.sum
+  a = 1
+  b = 2
+  c = []
+
+  while a < 4_000_000
+    c.push(a)
+    a, b = b, a + b
+  end
+
+  c.select(&:even?).sum
 end
-
-# Alternative solution using variable
-
-# def solution
-#   sum_of_multiples = 0
-
-#   (0...1000).each { |x| sum_of_multiples += x if (x % 3).zero? || (x % 5).zero? }
-
-#   sum_of_multiples
-# end
